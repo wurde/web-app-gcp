1d84f1a92b9177c46dd6d578d681fd0b8bbc2f02
Andy Bettisworth <andy.bettisworth@gmail.com>
2020-07-19 02:55:25 -0500

    less

--- resources.tf
+++ resources.tf
@@ -1,18 +1,6 @@
 # Configure resources.
 
-module "aws" {
-  count  = var.cloud == "aws" ? 1 : 0
-  source = "./src/aws"
-
-  dist_dir          = var.dist_dir
-  domain            = var.domain
-  alias_domains     = var.alias_domains
-  default_ttl       = var.default_ttl
-  country_blacklist = var.country_blacklist
-}
-
 module "gcp" {
-  count  = var.cloud == "gcp" ? 1 : 0
   source = "./src/gcp"
 
   dist_dir          = var.dist_dir
@@ -20,13 +8,4 @@ module "gcp" {
   alias_domains     = var.alias_domains
   default_ttl       = var.default_ttl
   country_blacklist = var.country_blacklist
-}
-
-module "netlify" {
-  count  = var.cloud == "netlify" ? 1 : 0
-  source = "./src/netlify"
-
-  dist_dir  = var.dist_dir
-  repo_path = var.repo_path
-  command   = var.command
 }
